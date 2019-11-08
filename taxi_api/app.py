e8510933e9308c3194a7d086027bd14d34fdb6e5
st3faniaalt1ni <53140311+st3faniaalt1ni@users.noreply.github.com>
2019-11-08 20:03:56 +0200

    Delete app.py

--- taxi_api/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_pymongo import PyMongo
-
-
-app = Flask(__name__)
-# app.secret_key = "secret key"
-
-mongo0 = PyMongo(app, uri = 'mongodb://localhost:27018/discovery_database')
-mongo = PyMongo(app, uri = 'mongodb://localhost:27018/demo_api')
